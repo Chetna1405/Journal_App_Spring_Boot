8e0603ebb7675267b35da717025ca38a71dcbcd2
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-07-17 00:44:52 +0530

    Add Lombok

--- src/main/java/com/SpringBootLearn/JournalApp/entity/JournalEntry.java
+++ src/main/java/com/SpringBootLearn/JournalApp/entity/JournalEntry.java
@@ -1,13 +1,17 @@
 package com.SpringBootLearn.JournalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Date;
-
+@Data
 @Document(collection = "journal_entries")
+
 public class JournalEntry {
 
     @Id
@@ -16,36 +20,36 @@ public class JournalEntry {
     private String content ;
     private LocalDateTime date ;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDateTime date) {
+//        this.date = date;
+//    }
+//
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
 
 }
